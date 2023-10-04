a538c780c01c1f91a36ee35e2d02ad11368e96c8
tanwubin <644346160@qq.com>
2023-10-04 19:26:05 +0800

    hackrf_tx.c codestyle fix:clang-formatted

--- host/hackrf-tools/src/hackrf_tx.c
+++ host/hackrf-tools/src/hackrf_tx.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FREQ  900000000
+#define FREQ 900000000
 
 hackrf_device* device = NULL;
 unsigned char* mbuffer = "012345678901234567890123456789";
@@ -14,7 +14,7 @@ void init_hackrf()
 	hackrf_open(&device);
 
 	hackrf_set_sample_rate(device, 8000000);
-	hackrf_set_baseband_filter_bandwidth(device, 8000000*0.75);
+	hackrf_set_baseband_filter_bandwidth(device, 8000000 * 0.75);
 	hackrf_set_freq(device, FREQ);
 	hackrf_set_txvga_gain(device, 40);
 	hackrf_set_lna_gain(device, 40);
@@ -23,19 +23,19 @@ void init_hackrf()
 	printf("HACKRF init done.\n");
 }
 
-int _hackrf_tx_callback(hackrf_transfer *transfer) 
+int _hackrf_tx_callback(hackrf_transfer* transfer)
 {
 	printf("transfer->valid_length is %d\n", transfer->valid_length);
 	// memcpy(transfer->buffer,mbuffer,transfer->valid_length);
 	if (strlen(mbuffer) >= transfer->valid_length) {
 		printf("send txt len overflow!\n");
 	}
-	memcpy(transfer->buffer,mbuffer,strlen(mbuffer));
+	memcpy(transfer->buffer, mbuffer, strlen(mbuffer));
 	printf("HACKRF send finish.%s\n", transfer->buffer);
 	return 0;
 }
 
-int main(void) 
+int main(void)
 {
 	for (int i = 0; i < 100; i++) {
 		init_hackrf();
